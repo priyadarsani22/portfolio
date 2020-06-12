fbbd3633bc0e6d1470fecd64e3adfab2ca106d74
Priyadarsani Padhy <priyadarsani1996@gmail.com>
2020-06-12 23:11:44 +0530

    added some features

--- js/script.js
+++ js/script.js
@@ -71,7 +71,29 @@ if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() 
 
 	});
 
-	
+	$("#navigation li a").click(function(e){
+		e.preventDefault();
+		var targetElement = $(this).attr("href");
+		var targetposition = $(targetElement).offset().top;
+		$("html, body").animate({scrollTop: targetposition - 50}, "slow")
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+		if($(window).scrollTop() >= navTop){
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav")
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav")
+		}
+
+	}
 
 
 });
